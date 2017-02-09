87e745e23e2e928412bb9dd5bb4764855932b7a3
Oleg <xdiver077@gmail.com>
2017-02-08 20:31:31 -0800

    Add dictionary example

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+
+dict = {'Joe': 1, 'Nick': 2, 'Emily': 3}
+
+print(dict['Joe'])
+
+dict['Joe'] = 15                                # update value
+
+print(dict['Joe'])
+
+del dict['Joe']
+
+# Remove all entries
+# dict.clear()
+# Remove dictionary
+# del dict
+
+print(dict.items())
+print(dict.keys())
+print(dict.values())
