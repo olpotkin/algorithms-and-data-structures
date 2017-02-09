9404e250eeb9d71262edd4c1f45cb1daea016569
olpotkin <olpotkin@gmail.com>
2017-02-09 11:08:33 -0800

    Add Depth-first Search algorithm implementation

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # algorithms-and-data-structures
+- Data structures:
 1. Array
 2. Linked list
 3. Stack
 4. Queue
 5. Binary Search Tree (BST)
+6. AVL Tree
+7. Heap
+8. Dictionary
+
+- Algorithms:
+1. Breadth-first Search
+2. Depth-first Search
---
+++ depth-first-search.py
@@ -0,0 +1,34 @@
+
+class Node(object):
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+        self.adjacencyList = []
+        self.visited = False
+        self.predecessor = None
+
+# BFS -> queue + layer by layer algorithm
+# DFS -> stack + goes as deep as possible
+class DepthFirstSearch(object):
+    def dfs(self, node):
+        node.visited = True;
+        print("{0}".format(node.name))
+
+        for n in node.adjacencyList:
+            if not n.visited:
+                self.dfs(n)
+
+# Testing
+node1 = Node("A")
+node2 = Node("B")
+node3 = Node("C")
+node4 = Node("D")
+node5 = Node("E")
+
+node1.adjacencyList.append(node2)
+node1.adjacencyList.append(node3)
+node2.adjacencyList.append(node4)
+node4.adjacencyList.append(node5)
+
+dfs = DepthFirstSearch()
+dfs.dfs(node1)
